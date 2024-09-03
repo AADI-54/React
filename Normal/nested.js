242991ae193ac89aa0c57244da0de95ae71a04b0
AADI-54 <aggarwaladitya839@gmail.com>
2024-09-03 20:49:52 +0530

    Day-1-part-1

---
+++ Normal/nested.js
@@ -0,0 +1,43 @@
+//<div id="parent">
+//   <div id="child">
+//        <h1>
+//
+//        </h1>
+//    </div>
+//</div>
+//nested elements using react
+
+const parent_ = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        React.createElement("h1", { id: "heading" }, "Nested tags")
+    )
+);
+const root_ = ReactDOM.createRoot(document.getElementById("root"));
+
+root_.render(parent_);
+
+//<div id="parent">
+//   <div id="child">
+//        <h1>
+//
+//        </h1>
+//        <h2>
+//        </h2>
+//    </div>
+//</div>
+//nested elements using react with siblings
+// in this case u will pass array at last arguments
+
+const parent = React.createElement("div", { id: "parent" },
+    React.createElement("div", { id: "child" },
+        [React.createElement("h1", { id: "heading" }, "Im h1 tag"), React.createElement("h2", { id: "heading2" }, "Im h2 tag")]
+    )
+);
+const root = ReactDOM.createRoot(document.getElementById("root2"));
+
+root.render(parent);
+
+// this way u can create siblings but it will give u
+//error saying u need to give it a key
+//by this way code will become complex so 
+// we will use jsx for this
---
+++ Readme.md
@@ -0,0 +1,5 @@
+React 
+Hello my name is Aditya, This readme contains my progess each day
+
+Day 1 : I created index.html , nested.js , hello.htm
+I learned about react create-element , create-root , And how to create nested elements. 
